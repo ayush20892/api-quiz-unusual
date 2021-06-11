1177486ffb73fff79e357e818050b726d0475129
ayush20892 <ayush20892@gmail.com>
2021-06-11 08:04:16 +0530

    feat: index file updated

--- index.js
+++ index.js
@@ -3,17 +3,38 @@ const app = express()
 var cors = require('cors')
 require('dotenv').config()
 
+// importing middlewares
+const { errorHandler } = require('./middlewares/errorHandler.js')
+const { routeNotFound } = require('./middlewares/routeNotFound.js')
 
-app.use(cors())
-const PORT = 8000
+
+// importing diffrent routers
+var quizRouter = require('./router/quiz.router.js')
+var userRouter = require('./router/user.router.js')
 
 
-app.use('/', (req,res) => {
+const { initializeDBConnection } = require('./db/db.connect.js')
+
+app.use(cors());
+
+initializeDBConnection()
+
+
+app.get('/', (req,res) => {
   res.send({ sucess: true, message: "Welcome to Quiz API"})
 })
 
 
+// Router Intialization
+app.use('/quiz', quizRouter)
+app.use('/user', userRouter)
+
+
+
+app.use(routeNotFound)
+
+app.use(errorHandler)
 
 app.listen(process.env.PORT, () => {
-  console.log(`App listening on port: ${PORT}`)
+  console.log(`App listening on port: ${process.env.PORT}`)
 })
